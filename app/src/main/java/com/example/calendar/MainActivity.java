79ed77383edc8255abcd486b0dac0ac1e0cfa323
02874712 <70228981+02874712@users.noreply.github.com>
2020-11-15 11:22:02 -0500

    Added Mel's code- main activity & etc.

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,8 +1,13 @@
-package com.example.calendar;
+package com.example.calendar;      //if red change to codepath4
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import com.example.calendar.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        final TextView selectedDay = findViewById(R.id.selectedDay);
+
+//        final TextView selectedYear = findViewById(R.id.selectedYear);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectedDay.setText("Day: " + dayOfMonth);
+
+            }
+        });
     }
 }
